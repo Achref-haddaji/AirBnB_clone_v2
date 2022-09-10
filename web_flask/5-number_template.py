f1797391da4a89f714edfe8151b2a9a4b02372f9
Achref-haddaji <haddaji.achref19@gmail.com>
2022-09-10 13:04:39 +0100

    task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,57 +1,47 @@
+
+
 #!/usr/bin/python3
-"""
-starts a Flask web application module
-"""
+""" flask module """
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb():
-    """
-        fn to display Hello HBNB!
-        in the route page
-    """
+def welcome_route():
+    """ Return hello HBNBH"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb_():
-    """
-        fn to display HBNB
-        in the route page
-    """
-    return 'HBN!'
+def simple_print_route():
+    """ return HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hbnb_c(text):
-    """
-        fn to display a text
-        in the route page
-    """
-    txt = text.replace('_', ' ')
-    return 'C {}'.format(txt)
+def c_route(text):
+    """ Return C is and input """
+    return 'C %s' % text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    """ return python is and input"""
+    return 'python %s' % text.replace("_", " ")
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def hbnb_n(n):
-    """
-        fn to display an integer n
-        in the route page
-    """
-    return '{} is a number'.format(n)
+def number_route(n):
+    """ check if n is an int """
+    return "%i is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def hbnb_html(n):
-    """
-        fn to display display a
-        HTML page only if n is an integer
-    """
+def number_template_route(n):
+    """ return html page if n is integer """
     return render_template('5-number.html', n=n)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
